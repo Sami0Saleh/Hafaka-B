ae19604b3ba752cb0a734ede63d925db2716ee43
yoavk-sudo <kenkendler123@gmail.com>
2024-07-20 02:51:05 +0300

    added scroll blockers

--- Hafaka B/Assets/Scripts/Camera/Scroll Camera.cs
+++ Hafaka B/Assets/Scripts/Camera/Scroll Camera.cs
@@ -4,6 +4,7 @@ public class ScrollCamera : MonoBehaviour
 {
     [SerializeField] bool _isLeftScroller;
     [SerializeField] ScrollCamera[] _scrollers;
+    [SerializeField] GameObject _boundary;
 
     private int _direction = 1;
     private Camera _camera;
@@ -24,10 +25,19 @@ public class ScrollCamera : MonoBehaviour
 
     private void OnMouseOver()
     {
+        if (IsAheadOfBoundary()) return;
         foreach (var scroller in _scrollers)
         {
             scroller.transform.position = new Vector2(scroller.transform.position.x + 1 * _direction * Time.deltaTime, scroller.transform.position.y);
         }
         _camera.transform.position = new Vector3(_camera.transform.position.x + 1 * _direction * Time.deltaTime, _camera.transform.position.y, _camera.transform.position.z);
+    }
+
+    private bool IsAheadOfBoundary()
+    {
+        if (_direction == 1)
+            return transform.position.x >= _boundary.transform.position.x;
+        else
+            return transform.position.x <= _boundary.transform.position.x;
     }
 }
