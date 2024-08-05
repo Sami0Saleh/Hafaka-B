73e3913ad6e2491dffe4faa7a10b42f69691f594
yoavk-sudo <kenkendler123@gmail.com>
2024-08-05 17:36:30 +0300

    ;ll,

--- Hafaka B/Assets/Scripts/Characters/CharacterScriptableObject.cs
+++ Hafaka B/Assets/Scripts/Characters/CharacterScriptableObject.cs
@@ -17,7 +17,7 @@ public class CharacterScriptableObject : ScriptableObject
     [SerializeField] Sprite _idImage;
     [SerializeField] string _firstName;
     [SerializeField] string _lastName;
-    [SerializeField] string _gender;
+    [SerializeField] Genders _gender;
     [SerializeField] string _dateOfBirth;
     [SerializeField] string _department;
     [SerializeField] string _jobTitle;
@@ -29,7 +29,7 @@ public class CharacterScriptableObject : ScriptableObject
     public Sprite BloodPool { get => _bloodPool; }
     public string FirstName { get => _firstName; }
     public string LastName { get => _lastName; }
-    public string Gender { get => _gender;  }
+    public Genders Gender { get => _gender;  }
     public string DateOfBirth { get => _dateOfBirth; }
     public string Department { get => _department; }
     public string JobTitle { get => _jobTitle; }
@@ -45,4 +45,11 @@ public class CharacterScriptableObject : ScriptableObject
     [field: SerializeField] public bool IsPositionWrong { get; set; } = false;
     [field: SerializeField] public bool IsDateOfBirthWrong { get; set; } = false;
     #endregion
+}
+
+public enum Genders
+{
+    Male,
+    Female,
+    NonBinary
 }
