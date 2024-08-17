39149116c2b3d2ee55de23f2189558ee13583365
yoavk-sudo <kenkendler123@gmail.com>
2024-08-17 11:33:19 +0300

    blood done

--- Hafaka B/Assets/Scripts/Managers/BloodSpawner.cs
+++ Hafaka B/Assets/Scripts/Managers/BloodSpawner.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class BloodSpawner : MonoBehaviour
 {
-    [SerializeField] SpriteRenderer _sprite;
+    [SerializeField] GameObject _sprite;
     [SerializeField] List<Sprite> _bloodImages;
-    [SerializeField] List<Material> _materials;
-    [SerializeField] float _spawnYPosition = -2.5f;
+    [SerializeField] float _spawnYPosition = -2.3f;
+    [SerializeField] List<Color> _colors = new List<Color>();
 
-    private Material _redMaterial;
+    private Color _redColor;
 
     public static BloodSpawner Instance { get; private set; }
 
@@ -26,20 +26,20 @@ public class BloodSpawner : MonoBehaviour
 
     private void Start()
     {
-        _redMaterial = _materials.FirstOrDefault(m => m.name.Contains("Red"));
+        _redColor = Color.red;
     }
 
     public void SpawnBlood(CharacterController character)
     {
-        GameObject bloodSplatter = Instantiate(_sprite.gameObject);
+        GameObject bloodSplatter = Instantiate(_sprite);
         bloodSplatter.transform.position = new Vector3(character.transform.position.x, _spawnYPosition, 0);
         SpriteRenderer sr = bloodSplatter.GetComponent<SpriteRenderer>();
         sr.sprite = _bloodImages[Random.Range(0, _bloodImages.Count - 1)];
-        if(character.IsAlien)
+        if(!character.IsAlien)
         {
-            sr.material = _redMaterial;
+            sr.color = _redColor;
             return;
         }
-        sr.material = _materials[Random.Range(0, _materials.Count - 1)];
+        sr.color = _colors[Random.Range(0, _colors.Count - 1)];
     }
 }
