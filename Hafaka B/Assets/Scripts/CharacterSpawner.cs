604562453a547eb8bd7f23476f9334fb1612efbc
SummerNova <137454229+SummerNova@users.noreply.github.com>
2024-09-22 18:25:20 +0300

    technically done, but for some reason it gets stuck

--- Hafaka B/Assets/Scripts/CharacterSpawner.cs
+++ Hafaka B/Assets/Scripts/CharacterSpawner.cs
@@ -8,8 +8,8 @@ public class CharacterSpawner : MonoBehaviour
     private int _spawnCount = 0;
     [SerializeField] List<CharacterScriptableObject> _workers;
     [SerializeField] List<CharacterScriptableObject> _workersSus;
-    [SerializeField] List<CharacterScriptableObject> _workersAlian;
-    [SerializeField] List<CharacterScriptableObject> _workersSubtleAlian;
+    [SerializeField] List<CharacterScriptableObject> _workersAlien;
+    [SerializeField] List<CharacterScriptableObject> _workersSubtleAlien;
     [SerializeField] GameObject _characterPrefab;
     [SerializeField] ExpectedListUI _expectedListUI;
     [SerializeField] Transform _lastGoal;
@@ -17,22 +17,45 @@ public class CharacterSpawner : MonoBehaviour
     [Header("Stats")]
     [SerializeField] int _minExpectedWorkers = 2;
     [SerializeField] int _maxExpectedWorkers = 5;
+    [SerializeField] int _MinUnexpectedWorkers = 2;
+    [SerializeField] int _MaxUnexpectedworkers = 5;
+    [SerializeField] int _MinAlienArrivals = 4;
+    [SerializeField] int _MaxAlienArrivals = 8;
+    [SerializeField,Range(0,1)] float _HumanSusChance = 0.3f;
+    [SerializeField, Range(0,1)] float _AlienObvChance = 0.3f;
     [SerializeField] float _minSpawnTime = 20f;
     [SerializeField] float _maxSpawnTime = 25f;
 
     public List<CharacterScriptableObject> _expectedWorkers = new List<CharacterScriptableObject>();
+    public List<int> _ExpectedWorkersIndexes = new List<int>();
+    private List<int> _SpawnedHumans;
     private float _secondsToWait;
 
-    public int CharacterCount { get; private set; }
+    private int ExpectedWorkersLeft;
+    private int UnexpectedLeft;
+    private int AliensLeft;
+
+    private int _characterCount;
+
+    public int CharacterCount { get { return _characterCount; } private set { _characterCount = value; } }
+
     public int SpawnCount { get => _spawnCount; set => _spawnCount = value; }
     public int count = 15;
     void Start()
     {
-        CharacterCount = Random.Range(_minExpectedWorkers, _maxExpectedWorkers + 1);
+        ExpectedWorkersLeft = Random.Range(_minExpectedWorkers, _maxExpectedWorkers + 1);
+        
+        SelectExpectedWorkers(ExpectedWorkersLeft);
+
+        UnexpectedLeft = Random.Range(_MinUnexpectedWorkers, _MaxUnexpectedworkers + 1);
+
+        AliensLeft = Random.Range(_MinAlienArrivals, _MaxAlienArrivals + 1);
+
+        CharacterCount = ExpectedWorkersLeft + UnexpectedLeft + AliensLeft;
+
         Debug.Log("will spawn " + CharacterCount);
-        SelectExpectedWorkers(CharacterCount);
         _expectedListUI.NewUpdateExpectedList(_expectedWorkers); // Update the UI
-        StartCoroutine(SpawnCharactersCoroutine(CharacterCount));
+        StartCoroutine(SpawnCharactersCoroutine());
     }
 
     private void SelectExpectedWorkers(int numberOfExpectedWorkers)
@@ -42,67 +65,37 @@ public class CharacterSpawner : MonoBehaviour
         for (int i = 0; i < numberOfExpectedWorkers; i++)
         {
             int randomIndex = Random.Range(0, workersPool.Count);
+            while (!_ExpectedWorkersIndexes.Contains(randomIndex)) {
+                randomIndex = Random.Range(0, workersPool.Count);
+            }
             CharacterScriptableObject selectedWorker = workersPool[randomIndex];
-                _expectedWorkers.Add(selectedWorker);
-                workersPool.RemoveAt(randomIndex);
-            
+            _expectedWorkers.Add(selectedWorker);
+            _ExpectedWorkersIndexes.Add(randomIndex);
         }
     }
 
 
-    private IEnumerator SpawnCharactersCoroutine(int numberOfExpectedWorkers)
+    private IEnumerator SpawnCharactersCoroutine()
     {
-        List<CharacterScriptableObject> spawnWorkerPool = new List<CharacterScriptableObject>(_workers);
-        List<CharacterScriptableObject> spawnWorkerSusPool = new List<CharacterScriptableObject>(_workersSus);
-        List<CharacterScriptableObject> spawnWorkerAlianPool = new List<CharacterScriptableObject>(_workersAlian);
-        List<CharacterScriptableObject> spawnWorkerSubtleAlianPool = new List<CharacterScriptableObject>(_workersSubtleAlian);
+        
 
-        CharacterScriptableObject randomCharacter;
-        for (int i = 0; i < numberOfExpectedWorkers; i++)
+        while(CharacterCount > 0 && UnexpectedLeft>0 && AliensLeft>0)
         {
-            
-            int randomIndex = Random.Range(0, count);
-            int randomNum = Random.Range(0, 4);
-            switch (randomNum)
-            {
-                case 0:
-                    randomCharacter = spawnWorkerPool[randomIndex];
-                    SpawnCharacter(randomCharacter);
-                    spawnWorkerPool.RemoveAt(randomIndex);
-                    spawnWorkerSusPool.RemoveAt(randomIndex);
-                    break;
-                case 1:
-                    randomCharacter = spawnWorkerSusPool[randomIndex];
-                    SpawnCharacter(randomCharacter);
-                    spawnWorkerPool.RemoveAt(randomIndex);
-                    spawnWorkerSusPool.RemoveAt(randomIndex);
-                    break;
-                case 2:
-                    randomCharacter = spawnWorkerAlianPool[randomIndex];
-                    SpawnCharacter(randomCharacter);
-                    spawnWorkerAlianPool.RemoveAt(randomIndex);
-                    spawnWorkerSubtleAlianPool.RemoveAt(randomIndex);
-                    break;
-                case 3:
-                    randomCharacter = spawnWorkerSubtleAlianPool[randomIndex];
-                    SpawnCharacter(randomCharacter);
-                    spawnWorkerAlianPool.RemoveAt(randomIndex);
-                    spawnWorkerSubtleAlianPool.RemoveAt(randomIndex);
-                    break;
-                default:
-                    break;
-            }
-            count--;
-            _secondsToWait = Random.Range(_minSpawnTime, _maxSpawnTime) - i * 2;
+            isExpectedSO Character = GetNextCharacter(); 
+
+            SpawnCharacter(Character._CharacterScriptableObject, Character._IsExpected);
+            _secondsToWait = Random.Range(_minSpawnTime, _maxSpawnTime);
+            _minSpawnTime = Mathf.Max(_minSpawnTime * 0.9f, 3);
+            _maxSpawnTime = Mathf.Max(_maxSpawnTime * 0.9f, 5);
             yield return new WaitForSeconds(_secondsToWait);
         }
     }
 
-    private void SpawnCharacter(CharacterScriptableObject characterSO)
+    private void SpawnCharacter(CharacterScriptableObject characterSO,bool isExpected)
     {
         GameObject character = Instantiate(_characterPrefab);
         CharacterController characterController = character.GetComponent<CharacterController>();
-        characterController.Init(characterSO);
+        characterController.Init(characterSO,isExpected);
 
         // Position the character randomly on the screen
         Vector2 position = transform.position;
@@ -112,4 +105,134 @@ public class CharacterSpawner : MonoBehaviour
         characterController.SetGoal(_lastGoal);
         SpawnCount++;
     }
+
+    private isExpectedSO GetNextCharacter()
+    {
+        int rnd;
+        bool run = true;
+        isExpectedSO output = new();
+        while (run)
+        {
+            rnd = Random.Range(0, 3);
+            switch (rnd)
+            {
+                case 0:
+                    if (AliensLeft > 0)
+                    {
+                        output = AddAliens();
+                        run = false;
+                    }
+                    break;
+                case 1:
+                    if (CharacterCount > 0)
+                    {
+                        output = AddExpectedHumans();
+                        run = false;
+                    }
+                    break;  
+                case 2:
+                    if (UnexpectedLeft > 0)
+                    {
+                        output = AddUnexpectedHumans();
+                        run = false;
+                    }
+                    break;
+            }
+        }
+        return output;
+    }
+
+    private isExpectedSO AddExpectedHumans()
+    {
+        int randomIndex;
+        float Chance;
+        randomIndex = Random.Range(0, _ExpectedWorkersIndexes.Count);
+        while (!_SpawnedHumans.Contains(_ExpectedWorkersIndexes[randomIndex]))
+        {
+            randomIndex = Random.Range(0, _ExpectedWorkersIndexes.Count);
+        }
+        _SpawnedHumans.Add(_ExpectedWorkersIndexes[randomIndex]);
+        Chance = Random.Range(0, 1);
+        CharacterCount--;
+        if (Chance < _HumanSusChance)
+        {
+            return new(_workersSus[randomIndex], true);
+        }
+        else
+        {
+            return new(_workers[randomIndex], true);
+        }
+        
+    }
+
+    private isExpectedSO AddUnexpectedHumans()
+    {
+        int randomIndex;
+        float Chance;
+        
+        randomIndex = Random.Range(0, _workers.Count);
+        while (!_SpawnedHumans.Contains(randomIndex) && !_ExpectedWorkersIndexes.Contains(randomIndex))
+        {
+            randomIndex = Random.Range(0, _workers.Count);
+        }
+        _SpawnedHumans.Add(randomIndex);
+        Chance = Random.Range(0, 1);
+        UnexpectedLeft--;
+        if (Chance < _HumanSusChance)
+        {
+            return new(_workersSus[randomIndex], false);
+        }
+        else
+        {
+            return new(_workers[randomIndex], false);
+        }
+        
+        
+    }
+
+    private isExpectedSO AddAliens()
+    {
+        int randomIndex;
+        bool isExpected;
+        float Chance;
+        
+        randomIndex = Random.Range(0, _workers.Count);
+        if (_ExpectedWorkersIndexes.Contains(randomIndex))
+        {
+            isExpected = true;
+        }
+        else
+        {
+            isExpected = false;
+        }
+        Chance = Random.Range(0, 1);
+        AliensLeft--;
+        if (Chance < _AlienObvChance)
+        {
+            return new(_workersAlien[randomIndex], isExpected);
+        }
+        else
+        {
+            return new(_workersSubtleAlien[randomIndex], isExpected);
+        }
+        
+    }
+}
+
+public class isExpectedSO
+{
+    public CharacterScriptableObject _CharacterScriptableObject;
+    public bool _IsExpected;
+
+    public isExpectedSO(CharacterScriptableObject SO, bool isExpected)
+    {
+        _CharacterScriptableObject = SO;
+        _IsExpected = isExpected;
+    }
+
+    public isExpectedSO()
+    {
+
+    }
+
 }
